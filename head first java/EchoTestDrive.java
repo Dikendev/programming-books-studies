f03cc44c7646e7f7ff1d61c9dbd78995fbbf1f8a
Dikendev <diken.dev@gmail.com>
2023-02-14 23:58:42 -0300

    chapter 2 EchoTestDrive

---
+++ head first java/EchoTestDrive.java
@@ -0,0 +1,31 @@
+public class EchoTestDrive {
+    public static void main (String[] args) {
+        Echo e1 = new Echo();
+        Echo e2 = new Echo();
+
+        int x = 0;
+
+        while (x < 4) {
+            e1.hello();
+            e1.count++;
+
+            if ( x == 3) {
+                e2.count++;
+            }
+            if (x > 0) {
+                e2.count = e2.count + e1.count;
+            }
+            x++;
+        }
+        System.out.println(e2.count);
+    }
+
+
+    static class Echo {
+        int count= 0;
+
+        void hello() {
+            System.out.println("helloooo.... ");
+        }
+    }
+}
