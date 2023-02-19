ac08d0ba6e5f3e0d71542ca9705429628d7a0d43
Dikendev <diken.dev@gmail.com>
2023-02-19 17:32:11 -0300

    chapter 4 CalcArea (Sharpen your pencil)

---
+++ head first java/CalcArea.java
@@ -0,0 +1,11 @@
+public class CalcArea {
+
+    static int calcArea(int height, int width) {
+        return height * width;
+    }
+    public static void main(String[] args) {
+
+        int a = calcArea(7,12);
+        System.out.println(a);
+    }
+}
