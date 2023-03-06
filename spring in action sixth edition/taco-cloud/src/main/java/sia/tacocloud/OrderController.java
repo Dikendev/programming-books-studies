9d9d31f2c20024618005000ea3cd88dbe559d83e
Dikendev <diken.dev@gmail.com>
2023-03-06 18:59:13 -0300

    added OrderController class simple without persistence

---
+++ spring in action sixth edition/taco-cloud/src/main/java/sia/tacocloud/OrderController.java
@@ -0,0 +1,28 @@
+package sia.tacocloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+
+    public String processOrder(TacoOrder order,
+                               SessionStatus sessionStatus) {
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+
+        return  "redirect:/";
+    }
+}
