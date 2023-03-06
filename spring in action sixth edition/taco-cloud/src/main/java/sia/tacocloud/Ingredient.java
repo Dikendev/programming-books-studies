3159278207019cffb3c860f036fd344e435d1534
Dikendev <diken.dev@gmail.com>
2023-03-06 13:13:38 -0300

    fixed sauce misspelled

--- spring in action sixth edition/taco-cloud/src/main/java/sia/tacocloud/Ingredient.java
+++ spring in action sixth edition/taco-cloud/src/main/java/sia/tacocloud/Ingredient.java
@@ -9,6 +9,6 @@ public class Ingredient {
     private final Type type;
 
     public enum Type {
-        WRAP, PROTEIN, VEGGIES, CHEESE, SAUSE
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
 }
