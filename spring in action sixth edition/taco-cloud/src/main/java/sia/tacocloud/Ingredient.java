23c4b3dfb57904613c50768add763f8dc4487921
Dikendev <diken.dev@gmail.com>
2023-03-05 23:18:55 -0300

    added Ingredient class defining the domain object

---
+++ spring in action sixth edition/taco-cloud/src/main/java/sia/tacocloud/Ingredient.java
@@ -0,0 +1,14 @@
+package sia.tacocloud;
+
+import lombok.Data;
+
+@Data
+public class Ingredient {
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUSE
+    }
+}
