63f4551ba0429db3574a7456cb49961efcf9b029
Dikendev <diken.dev@gmail.com>
2023-03-05 23:31:09 -0300

    added TacoOrder class

---
+++ spring in action sixth edition/taco-cloud/src/main/java/sia/tacocloud/TacoOrder.java
@@ -0,0 +1,25 @@
+package sia.tacocloud;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class TacoOrder {
+
+    private String deliveryName;
+    private String deliveryStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
+
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addTaco(Taco taco) {
+        this.tacos.add(taco);
+    }
+}
