42dea994705bf7016e59ddff675d5ecc0bb8b5f9
Dikendev <diken.dev@gmail.com>
2023-03-07 23:59:34 -0300

    implemented error handler validation

--- spring in action sixth edition/taco-cloud/src/main/java/sia/tacocloud/OrderController.java
+++ spring in action sixth edition/taco-cloud/src/main/java/sia/tacocloud/OrderController.java
@@ -1,8 +1,11 @@
 package sia.tacocloud;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
@@ -18,8 +21,12 @@ public class OrderController {
         return "orderForm";
     }
 
-    public String processOrder(TacoOrder order,
+    @PostMapping
+    public String processOrder(@Valid TacoOrder order, Errors errors,
                                SessionStatus sessionStatus) {
+        if(errors.hasErrors()) {
+            return "orderForm";
+        }
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
 
