13b9c3fd8b5ffc688776dd7a6b79fa48731e6d1a
Dikendev <diken.dev@gmail.com>
2023-03-07 23:54:08 -0300

    implemented NotNull annotation to ingredients list

--- spring in action sixth edition/taco-cloud/src/main/java/sia/tacocloud/Taco.java
+++ spring in action sixth edition/taco-cloud/src/main/java/sia/tacocloud/Taco.java
@@ -1,5 +1,7 @@
 package sia.tacocloud;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
@@ -7,6 +9,10 @@ import java.util.List;
 @Data
 public class Taco {
 
+    @NotNull
+    @Size(min=5, message = "Name must be at least 5 characters long")
     private String name;
+    @NotNull
+    @Size(min=1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 }
