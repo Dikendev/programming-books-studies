8b0854786b31e32fa82ef320017d0463aa9be189
Dikendev <diken.dev@gmail.com>
2023-03-19 02:00:14 -0300

    added IngredientRepository interface

---
+++ spring in action sixth edition/taco-cloud/src/main/java/sia/tacocloud/repositories/IngredientRepository.java
@@ -0,0 +1,11 @@
+package sia.tacocloud.repositories;
+
+import sia.tacocloud.Ingredient;
+
+import java.util.Optional;
+
+public interface IngredientRepository {
+    Iterable<Ingredient> findAll();
+    Optional<Ingredient> findById(String id);
+    Ingredient save(Ingredient ingredient);
+}
