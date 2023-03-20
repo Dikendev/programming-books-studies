e8bf72356357e32513a25cc7bad434bc206f0562
Dikendev <diken.dev@gmail.com>
2023-03-19 23:41:00 -0300

    refactored inventory, more modular data

--- spring in action sixth edition/taco-cloud/src/main/java/sia/tacocloud/IngredientByIdConverter.java
+++ spring in action sixth edition/taco-cloud/src/main/java/sia/tacocloud/IngredientByIdConverter.java
@@ -1,43 +1,22 @@
 package sia.tacocloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import sia.tacocloud.Ingredient.Type;
+import sia.tacocloud.repositories.IngredientRepository;
 
 @Component
 public class IngredientByIdConverter implements Converter<String, Ingredient> {
 
-    private Map<String, Ingredient> ingredientMap = new HashMap<>();
+    private IngredientRepository ingredientRepository;
 
-    public IngredientByIdConverter() {
-        ingredientMap.put("FLTO",
-                new Ingredient("FLTO","Flour Tortilla", Type.WRAP));
-        ingredientMap.put("COTO",
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
-        ingredientMap.put("GRBF",
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
-        ingredientMap.put("CARN",
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN));
-        ingredientMap.put("TMTO",
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
-        ingredientMap.put("LETC",
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES));
-        ingredientMap.put("CHED",
-                new Ingredient("CHED", "Cheddar", Type.CHEESE));
-        ingredientMap.put("JACK",
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
-        ingredientMap.put("SLSA",
-                new Ingredient("SLSA", "Salsa", Type.SAUCE));
-        ingredientMap.put("SRCR",
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+    @Autowired
+    public IngredientByIdConverter(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
     }
 
     @Override
     public Ingredient convert(String id) {
-        return ingredientMap.get(id);
+        return ingredientRepository.findById(id).orElse(null);
     }
 }
