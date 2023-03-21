a4f3b157e373c091b09870543a535bed3f80a155
Dikendev <diken.dev@gmail.com>
2023-03-21 16:17:27 -0300

    refactored Type variable enum

--- spring in action sixth edition/taco-cloud/src/main/java/sia/tacocloud/entities/Ingredient.java
+++ spring in action sixth edition/taco-cloud/src/main/java/sia/tacocloud/entities/Ingredient.java
@@ -1,14 +1,11 @@
-package sia.tacocloud;
+package sia.tacocloud.entities;
 
 import lombok.Data;
+import sia.tacocloud.entities.enuns.Type;
 
 @Data
 public class Ingredient {
     private final String id;
     private final String name;
     private final Type type;
-
-    public enum Type {
-        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
-    }
 }
