284d05b3f204c8cc9907ed7ed28fe06d2ee414b1
Dikendev <diken.dev@gmail.com>
2023-03-31 23:42:47 -0300

    MongoDB mapping

--- spring in action sixth edition/taco-cloud/src/main/java/sia/tacocloud/repositories/IngredientRepository.java
+++ spring in action sixth edition/taco-cloud/src/main/java/sia/tacocloud/repositories/IngredientRepository.java
@@ -1,10 +1,12 @@
 package sia.tacocloud.repositories;
 
-import sia.tacocloud.Ingredient;
+import org.springframework.data.repository.CrudRepository;
+import sia.tacocloud.entities.Ingredient;
 
 import java.util.Optional;
 
-public interface IngredientRepository {
+public interface IngredientRepository extends CrudRepository<Ingredient,String> {
+
     Iterable<Ingredient> findAll();
     Optional<Ingredient> findById(String id);
     Ingredient save(Ingredient ingredient);
--- spring in action sixth edition/taco-cloud/src/main/java/sia/tacocloud/repositories/OrderRepository.java
+++ spring in action sixth edition/taco-cloud/src/main/java/sia/tacocloud/repositories/OrderRepository.java
@@ -1,7 +1,14 @@
 package sia.tacocloud.repositories;
 
+import org.springframework.data.repository.CrudRepository;
 import sia.tacocloud.entities.TacoOrder;
 
-public interface OrderRepository {
+import java.util.Date;
+import java.util.List;
+
+public interface OrderRepository extends CrudRepository<TacoOrder, String> {
+
     TacoOrder save(TacoOrder order);
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+    List<TacoOrder> readOrdersByDeliveryZipAndPlaceAtBetween(String deliveyZip, Date startDate, Date EndDate);
 }
