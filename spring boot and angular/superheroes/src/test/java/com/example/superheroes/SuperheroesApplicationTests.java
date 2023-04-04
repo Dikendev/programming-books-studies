0e99e14ae0b4bc8107a70ef045939db8994e4cd4
Dikendev <diken.dev@gmail.com>
2023-04-04 00:42:25 -0300

    initial commit chapter 5

---
+++ spring boot and angular/superheroes/src/test/java/com/example/superheroes/SuperheroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superheroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
