459899185ba711b2c8b49fc1e9dde316cd54d1b9
Dikendev <diken.dev@gmail.com>
2023-04-05 23:57:16 -0300

    Controller convert entity to a DTO , vice versa

--- spring boot and angular/superheroes/src/main/java/com/example/superheroes/antiHero/controller/AntiHeroController.java
+++ spring boot and angular/superheroes/src/main/java/com/example/superheroes/antiHero/controller/AntiHeroController.java
@@ -1,6 +1,10 @@
 package com.example.superheroes.antiHero.controller;
 
+import com.example.superheroes.antiHero.dto.AntiHeroDto;
+import com.example.superheroes.antiHero.entity.AntiHeroEntity;
+import com.example.superheroes.antiHero.service.AntiHeroService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/anti-heroes")
 public class AntiHeroController {
+
+    private final AntiHeroService heroService;
+
+    private final ModelMapper modelMapper;
+
+    private AntiHeroDto convertToDto(AntiHeroEntity entity) {
+        return modelMapper.map(entity, AntiHeroDto.class);
+    }
+
+    private AntiHeroEntity convertToEntity(AntiHeroDto dto) {
+        return modelMapper.map(dto, AntiHeroEntity.class);
+    }
 }
