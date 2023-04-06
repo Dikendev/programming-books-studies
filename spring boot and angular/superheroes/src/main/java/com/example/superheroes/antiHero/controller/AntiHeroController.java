f6b1074738ad6414dadc441e91fe72122c982dca
Dikendev <diken.dev@gmail.com>
2023-04-05 23:50:55 -0300

    added spring web / modelmapper

---
+++ spring boot and angular/superheroes/src/main/java/com/example/superheroes/antiHero/controller/AntiHeroController.java
@@ -0,0 +1,11 @@
+package com.example.superheroes.antiHero.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("api/v1/anti-heroes")
+public class AntiHeroController {
+}
