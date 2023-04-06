79e338c56100109fa55ae936ce23b168a6eb5389
Dikendev <diken.dev@gmail.com>
2023-04-06 02:05:55 -0300

    Controller

--- spring boot and angular/superheroes/src/main/java/com/example/superheroes/antiHero/controller/AntiHeroController.java
+++ spring boot and angular/superheroes/src/main/java/com/example/superheroes/antiHero/controller/AntiHeroController.java
@@ -3,10 +3,14 @@ package com.example.superheroes.antiHero.controller;
 import com.example.superheroes.antiHero.dto.AntiHeroDto;
 import com.example.superheroes.antiHero.entity.AntiHeroEntity;
 import com.example.superheroes.antiHero.service.AntiHeroService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
 
 @AllArgsConstructor
 @RestController
@@ -24,4 +28,36 @@ public class AntiHeroController {
     private AntiHeroEntity convertToEntity(AntiHeroDto dto) {
         return modelMapper.map(dto, AntiHeroEntity.class);
     }
+
+    @GetMapping("/{id}")
+    public AntiHeroDto getAntiHeroById(@PathVariable("id") UUID id) {
+        return convertToDto(heroService.findAntiHeroById(id));
+    }
+
+    @PostMapping
+    public AntiHeroDto postAntiHero(@Valid @RequestBody AntiHeroDto antiHeroDto) {
+        var entity = convertToEntity(antiHeroDto);
+        var antiHero = heroService.addAntiHero(entity);
+
+        return convertToDto(antiHero);
+    }
+
+    @PutMapping("/{id}")
+    public void putAntiHero(@PathVariable("id") UUID id,
+                            @Valid @RequestBody AntiHeroDto antiHeroDto) {
+        if (!id.equals(antiHeroDto.getId())) throw new
+                ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "id does not match."
+        );
+
+        var antiHeroEntity = convertToEntity(antiHeroDto);
+        heroService.updateAntiHero(id, antiHeroEntity);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAntiHeroById(@PathVariable("id") UUID id) {
+        heroService.removeAntiHeroById(id);
+    }
+
 }
