0cd0bf5377d6c4d1d69a4cd7cab6533ba3e9e784
Dikendev <diken.dev@gmail.com>
2023-04-07 17:03:42 -0300

    Redis / Swagger / OpenAPI config

---
+++ spring boot and angular/superheroes/src/main/java/com/example/superheroes/config/SpringDocConfig.java
@@ -0,0 +1,17 @@
+package com.example.superheroes.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI baseOpenAPI() {
+        return new OpenAPI().info(new Info().title("Spring doc").version("1.0.0").description("Spring doc"));
+    }
+}
