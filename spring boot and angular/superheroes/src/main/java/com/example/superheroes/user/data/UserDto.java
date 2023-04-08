aca2f30ac301f2c16c2ace175bbedf9e968f56f1
Dikendev <diken.dev@gmail.com>
2023-04-07 23:42:45 -0300

    User Entity / DTO / Repository

---
+++ spring boot and angular/superheroes/src/main/java/com/example/superheroes/user/data/UserDto.java
@@ -0,0 +1,18 @@
+package com.example.superheroes.user.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private UUID id;
+    private String email;
+    private String mobileNumber;
+    private String password;
+}
---
+++ spring boot and angular/superheroes/src/main/java/com/example/superheroes/user/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.example.superheroes.user.repository;
+
+import com.example.superheroes.user.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+    @Query(
+            " " + "SELECT CASE WHEN COUNT(u) > 0 THEN " +
+                    "TRUE ELSE FALSE END " +
+                    "FROM UserEntity u " +
+                    "WHERE U.email = ?1"
+    )
+    Boolean selectExistsEmail(String email);
+
+    UserEntity findByEmail(String email);
+}
