c67eba51dc742674c8f225b26dfe38767ce1bbae
Dikendev <diken.dev@gmail.com>
2023-04-08 09:51:27 -0300

    JWT SECRET_KEY

--- spring boot and angular/superheroes/src/main/java/com/example/superheroes/jwt/util/JwtUtil.java
+++ spring boot and angular/superheroes/src/main/java/com/example/superheroes/jwt/util/JwtUtil.java
@@ -1,8 +1,8 @@
-JWT creation, validation and expiration checks, and extraction of the informationpackage com.example.superheroes.jwt.util;
+package com.example.superheroes.jwt.util;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +10,11 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-import static javax.crypto.Cipher.SECRET_KEY;
-
 @Service
 public class JwtUtil {
+
+    @Value("${jw}")
+    private String SECRET_KEY;
 
     private String createToken(Map<String, Object> claims, String subject) {
         return Jwts
@@ -31,4 +32,6 @@ public class JwtUtil {
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, userDetails.getUsername());
     }
+
+
 }
