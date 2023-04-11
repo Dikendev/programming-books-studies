861b41bcdbca6f3b15ccab199585abc10aeb429b
Dikendev <diken.dev@gmail.com>
2023-04-11 00:59:17 -0300

    JUnit test using Mockito to find all Entity

--- spring boot and angular/superheroes/src/test/java/com/example/superheroes/antiHero/service/AntiHeroServiceTest.java
+++ spring boot and angular/superheroes/src/test/java/com/example/superheroes/antiHero/service/AntiHeroServiceTest.java
@@ -1,10 +1,13 @@
 package com.example.superheroes.antiHero.service;
 
 import com.example.superheroes.antiHero.repository.AntiHeroRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class AntiHeroServiceTest {
@@ -14,4 +17,13 @@ public class AntiHeroServiceTest {
 
     @InjectMocks
     private AntiHeroService antiHeroServiceTest;
+
+    @Test
+    void canFindAllAntiHeroes() {
+        //when
+        antiHeroServiceTest.findAllAntiHeroes();
+        //then
+        verify(antiHeroRepository).findAll();
+    }
+
 }
