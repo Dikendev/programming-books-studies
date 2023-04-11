751231ae3e07371dcaaddfed9f717df1f51323ef
Dikendev <diken.dev@gmail.com>
2023-04-11 16:28:46 -0300

    Component shared footer

--- spring boot and angular/frontend-angular/src/app/shared/shared.module.ts
+++ spring boot and angular/frontend-angular/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
