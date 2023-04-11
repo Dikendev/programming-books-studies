a5bae64057399f13aebadab980be17715b863587
Dikendev <diken.dev@gmail.com>
2023-04-11 01:00:25 -0300

    JUnit test using Mockito to add Entity through service

--- spring boot and angular/superheroes/src/test/java/com/example/superheroes/antiHero/service/AntiHeroServiceTest.java
+++ spring boot and angular/superheroes/src/test/java/com/example/superheroes/antiHero/service/AntiHeroServiceTest.java
@@ -1,12 +1,19 @@
 package com.example.superheroes.antiHero.service;
 
+import com.example.superheroes.antiHero.entity.AntiHeroEntity;
 import com.example.superheroes.antiHero.repository.AntiHeroRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,4 +33,28 @@ public class AntiHeroServiceTest {
         verify(antiHeroRepository).findAll();
     }
 
+    @Test
+    void canAddAntiHero() {
+        //give
+        AntiHeroEntity antiHeroEntity = new AntiHeroEntity(
+                UUID.randomUUID(),
+                "Diego",
+                "Kennedy",
+                "BNU",
+                "Diken",
+                new SimpleDateFormat("dd-MM-yyyy HH:mm:ss z").format(new Date())
+        );
+
+        //when
+        antiHeroServiceTest.addAntiHero(antiHeroEntity);
+
+        //then
+        ArgumentCaptor<AntiHeroEntity> antiHeroEntityArgumentCaptor = ArgumentCaptor
+                .forClass(AntiHeroEntity.class);
+
+        verify(antiHeroRepository).save(antiHeroEntityArgumentCaptor.capture());
+        AntiHeroEntity captureAntiHero = antiHeroEntityArgumentCaptor.getValue();
+
+        assertThat(captureAntiHero).isEqualTo(antiHeroEntity);
+    }
 }
