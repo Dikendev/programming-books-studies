e3da7b5d20d215c58d2ab5103f5b00de0b48a194
Dikendev <diken.dev@gmail.com>
2023-04-11 15:39:40 -0300

    lazy loading route for AntiHeroModule

--- spring boot and angular/frontend-angular/src/app/app-routing.module.ts
+++ spring boot and angular/frontend-angular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: " ",
+    redirectTo: "anti-heroes",
+    pathMatch: "full",
+  },
+  {
+    path: "anti-heroes",
+    loadChildren: () => import ("./anti-hero/anti-hero.module").then((m) => m.AntiHeroModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
