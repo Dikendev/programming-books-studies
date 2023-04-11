ff8b795ef095c19f402aacbf6632ee5615b3c535
Dikendev <diken.dev@gmail.com>
2023-04-11 00:47:51 -0300

    JUnit test using Mockito

---
+++ spring boot and angular/superheroes/src/test/java/com/example/superheroes/antiHero/service/AntiHeroServiceTest.java
@@ -0,0 +1,17 @@
+package com.example.superheroes.antiHero.service;
+
+import com.example.superheroes.antiHero.repository.AntiHeroRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AntiHeroServiceTest {
+
+    @Mock
+    private AntiHeroRepository antiHeroRepository;
+
+    @InjectMocks
+    private AntiHeroService antiHeroServiceTest;
+}
