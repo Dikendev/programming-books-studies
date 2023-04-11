1d77905ca5d77f4a562732d235a73db89128f0a4
Dikendev <diken.dev@gmail.com>
2023-04-10 23:45:06 -0300

    JUnit and AssertJ test User Repository find user by email

--- spring boot and angular/superheroes/src/test/java/com/example/superheroes/user/UserRepositoryTest.java
+++ spring boot and angular/superheroes/src/test/java/com/example/superheroes/user/UserRepositoryTest.java
@@ -3,8 +3,11 @@ package com.example.superheroes.user;
 import com.example.superheroes.user.entity.UserEntity;
 import com.example.superheroes.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
 public class UserRepositoryTest {
@@ -20,4 +23,27 @@ public class UserRepositoryTest {
         userTest.setMobileNumber("222");
     }
 
+     @Test
+     void itShouldCheckWhenUserEmailExist() {
+
+         repositoryTest.save(userTest);
+
+         //when
+         boolean expected = repositoryTest.selectExistsEmail(userTest.getEmail());
+
+         //then
+         assertThat(expected).isTrue();
+     }
+
+    @Test
+    void itShouldFindUserWhenEmailExist() {
+
+        repositoryTest.save(userTest);
+
+        //when
+        UserEntity expected = repositoryTest.findByEmail(userTest.getEmail());
+
+        //then
+        assertThat(expected).isEqualTo(userTest);
+    }
 }
