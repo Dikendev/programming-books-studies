bf67cb55d74fbbf1fd8a355b38d944e2a9f271dd
Dikendev <diken.dev@gmail.com>
2023-04-10 23:09:10 -0300

    JUnit and AssertJ test service to add and retrieve AntiHero entities.

--- spring boot and angular/superheroes/src/test/java/com/example/superheroes/antiHero/AntiHeroH2ServiceTest.java
+++ spring boot and angular/superheroes/src/test/java/com/example/superheroes/antiHero/AntiHeroH2ServiceTest.java
@@ -3,17 +3,12 @@ package com.example.superheroes.antiHero;
 import com.example.superheroes.antiHero.entity.AntiHeroEntity;
 import com.example.superheroes.antiHero.repository.AntiHeroRepository;
 import com.example.superheroes.antiHero.service.AntiHeroService;
-import com.example.superheroes.exception.NotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.UUID;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 public class AntiHeroH2ServiceTest {
