5fa226126c44109a64a2560322c55009b558f05d
Dikendev <diken.dev@gmail.com>
2023-04-23 23:58:18 -0300

    components and services

--- spring boot and angular/frontend-angular/src/app/anti-hero/anti-hero.module.ts
+++ spring boot and angular/frontend-angular/src/app/anti-hero/anti-hero.module.ts
@@ -4,13 +4,15 @@ import { AntiHeroRoutingModule } from './anti-hero-routing.module';
 import { ListComponent } from './pages/list/list.component';
 import { FormComponent } from './pages/pages/form/form.component';
 import { AntiHeroListComponent } from './components/anti-hero-list/anti-hero-list.component';
+import { AntiHeroFormComponent } from './components/anti-hero-form/anti-hero-form.component';
 
 
 @NgModule({
   declarations: [
     ListComponent,
     FormComponent,
-    AntiHeroListComponent
+    AntiHeroListComponent,
+    AntiHeroFormComponent
   ],
   imports: [
     CommonModule,
