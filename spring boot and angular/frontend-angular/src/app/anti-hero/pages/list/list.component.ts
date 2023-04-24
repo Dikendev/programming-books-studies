a0fc1e7f351c7f1b9601f111ee3568fcf5795e21
Dikendev <diken.dev@gmail.com>
2023-04-23 23:55:20 -0300

    definition of the headers

--- spring boot and angular/frontend-angular/src/app/anti-hero/pages/list/list.component.ts
+++ spring boot and angular/frontend-angular/src/app/anti-hero/pages/list/list.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AntiHero } from '../../models/anti-hero.interface';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.sass']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  antiHeroes: AntiHero[] = [
+    {
+      firstName: "Eddie",
+      lastName: "Brock",
+      house: "New York",
+      knownAs: "Venom"
+    }
+  ]
+  headers: {headerName: string, fieldName: keyof AntiHero}[]= [
+    {headerName: "First Name", fieldName: "firstName"},
+    {headerName: "Last Name", fieldName: "lastName"},
+    {headerName: "House", fieldName:"house"},
+    {headerName: "Know as", fieldName:"knownAs"}
+  ]
+  
+  constructor() { }
+  ngOnInit(): void {
+  }
 
+  selectAntiHero(antiHero:AntiHero) {}
 }
