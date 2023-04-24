d6903abb817cfaedde6e6cd28320553ff9325386
Dikendev <diken.dev@gmail.com>
2023-04-23 22:33:44 -0300

    core module

--- spring boot and angular/frontend-angular/src/app/core/core.module.ts
+++ spring boot and angular/frontend-angular/src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
@@ -9,4 +9,10 @@ import { CommonModule } from '@angular/common';
     CommonModule
   ]
 })
-export class CoreModule { }
+export class CoreModule { 
+  constructor (@Optional() @SkipSelf() core:CoreModule) {
+    if (core) {
+      throw new Error("Core module should only be imported to the Root Module")
+    }
+  }
+}
