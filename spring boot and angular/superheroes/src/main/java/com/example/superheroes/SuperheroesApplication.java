eed1c96785514d0e2152dcfb826ee0b592152023
Dikendev <diken.dev@gmail.com>
2023-05-08 23:54:08 -0300

    Lombok SLF4j

--- spring boot and angular/superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ spring boot and angular/superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,13 +1,22 @@
 package com.example.superheroes;
 
+import com.example.superheroes.antiHero.controller.AntiHeroController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SuperheroesApplication {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(AntiHeroController.class);
 	public static void main(String[] args) {
+
 		SpringApplication.run(SuperheroesApplication.class, args);
+		LOGGER.info("Using SLF4J: Getting anti-hero list - getAntiHeroes()");
+		//LOMBOK SLF4j
+
 	}
+
 
 }
