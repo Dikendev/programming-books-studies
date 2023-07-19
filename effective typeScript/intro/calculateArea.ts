be4b7ef7ec1e51b875b02134102cbe628958a035
Dikendev <diken.dev@gmail.com>
2023-07-18 23:15:02 -0300

    calculateArea 🔥

---
+++ effective typeScript/intro/calculateArea.ts
@@ -0,0 +1,19 @@
+interface Square {
+	kind: "square";
+	width: number;
+}
+interface Rectangle {
+	kind: "rectangle";
+	height: number;
+	width: number;
+}
+type Shape = Square | Rectangle;
+
+function calculateArea(shape: Shape) {
+	if (shape.kind === "rectangle") {
+		return shape.width * shape.height;
+	} else {
+		shape;
+		return shape.width * shape.width;
+	}
+}
