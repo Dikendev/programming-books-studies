10fa2680ed3da55687e039372b1398837a9e2a92
Dikendev <diken.dev@gmail.com>
2023-08-13 00:07:29 -0300

    rest parameters

--- typeScript/functions.ts
+++ typeScript/functions.ts
@@ -7,3 +7,17 @@ function disp_details(id: number, name: string, mail_id?: string) {
 
 disp_details(123, "Jhon");
 disp_details(111, "Mary", "mary@xyc.com");
+
+// Rest parameters
+
+function addNumbers(...nums: number[]) {
+	let sum: number = 0;
+
+	for (let i = 0; i < nums.length; i++) {
+		sum = sum + nums[i];
+	}
+	console.log("sum of the numbers", sum);
+}
+
+addNumbers(1, 2, 3);
+addNumbers(10, 10, 10, 10, 20);
