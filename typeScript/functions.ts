b48649cbd6b0f610881d2285dadf6a93b9c17e87
Dikendev <diken.dev@gmail.com>
2023-08-13 00:17:37 -0300

    Default Parameters

--- typeScript/functions.ts
+++ typeScript/functions.ts
@@ -1,6 +1,6 @@
 function disp_details(id: number, name: string, mail_id?: string) {
-	console.log("ID:", id);
-	console.log("Name", name);
+	// console.log("ID:", id);
+	// console.log("Name", name);
 
 	if (mail_id != undefined) console.log("Email id", mail_id);
 }
@@ -9,15 +9,23 @@ disp_details(123, "Jhon");
 disp_details(111, "Mary", "mary@xyc.com");
 
 // Rest parameters
-
 function addNumbers(...nums: number[]) {
 	let sum: number = 0;
 
 	for (let i = 0; i < nums.length; i++) {
 		sum = sum + nums[i];
 	}
-	console.log("sum of the numbers", sum);
+	// console.log("sum of the numbers", sum);
 }
 
 addNumbers(1, 2, 3);
 addNumbers(10, 10, 10, 10, 20);
+
+// Default parameters
+function calculate_discount(price: number, rate: number = 0.5) {
+	let discount = price * rate;
+	console.log("Discount Amount", discount);
+}
+
+calculate_discount(1000);
+calculate_discount(1000, 0.3);
