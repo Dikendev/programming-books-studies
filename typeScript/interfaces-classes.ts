c1d110c1845816552b07dd94b32a2352288afc4c
Dikendev <diken.dev@gmail.com>
2023-08-18 01:16:24 -0300

    interface + classes

--- typeScript/interfaces-classes.ts
+++ typeScript/interfaces-classes.ts
@@ -29,5 +29,39 @@ class Book {
 const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925);
 const book2 = new Book("To Kill a Mockingbird", "Harper Lee");
 
-console.log(book1.getDetails());
-console.log(book2.getDetails());
+// console.log(book1.getDetails());
+// console.log(book2.getDetails());
+
+interface Student {
+	id: number;
+	name: string;
+	courses: string[];
+}
+
+class School {
+	private students: Student[] = [];
+
+	enrollStudent(student: Student): void {
+		this.students.push(student);
+	}
+
+	getStudentNames(): string[] {
+		return this.students.map((student) => student.name);
+	}
+}
+
+const school = new School();
+const student1: Student = {
+	id: 1,
+	name: "Cris",
+	courses: ["Math", "Science"],
+};
+const student2: Student = {
+	id: 2,
+	name: "Diego",
+	courses: ["History", "English"],
+};
+
+school.enrollStudent(student1);
+school.enrollStudent(student2);
+console.log(school.getStudentNames());
