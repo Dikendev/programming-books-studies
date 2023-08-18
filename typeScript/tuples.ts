04508dbb4d872b2f101447d9ca92961c9a6ca012
Dikendev <diken.dev@gmail.com>
2023-08-18 00:16:43 -0300

    tuples

---
+++ typeScript/tuples.ts
@@ -0,0 +1,9 @@
+// Declaring a tuple
+let person: [string, number] = ["Alice", 30];
+
+// Accessing tuple elements
+let personName: string = person[0];
+let age: number = person[1];
+
+console.log(personName);
+console.log(age);
