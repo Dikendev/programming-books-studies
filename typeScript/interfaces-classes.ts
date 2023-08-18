dd97d42e389300869ac68ba6ee68775e1cd5b964
Dikendev <diken.dev@gmail.com>
2023-08-18 01:07:27 -0300

    classes

--- typeScript/interfaces-classes.ts
+++ typeScript/interfaces-classes.ts
@@ -10,4 +10,24 @@ function calculateDistanceFromOrigin(point: Point): number {
 
 const myPoint: Point = { x: 3, y: 4 };
 const distance = calculateDistanceFromOrigin(myPoint);
-console.log(distance);
+// console.log(distance);
+
+class Book {
+	constructor(
+		public title: string,
+		public author: string,
+		public publicationYear?: number
+	) {}
+
+	getDetails(): string {
+		return `${this.title} by ${this.author}${
+			this.publicationYear ? `, ${this.publicationYear}` : ""
+		}`;
+	}
+}
+
+const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925);
+const book2 = new Book("To Kill a Mockingbird", "Harper Lee");
+
+console.log(book1.getDetails());
+console.log(book2.getDetails());
