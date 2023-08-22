26948da293f0b58a8aac0890f2bf7f08828b2473
Dikendev <diken.dev@gmail.com>
2023-08-22 00:20:01 -0300

    counter

--- typeScript/counter.ts
+++ typeScript/counter.ts
@@ -5,26 +5,27 @@ type ReturnObj = {
 };
 
 function createCounter(init: number): ReturnObj | undefined {
+	let newVal = init;
+
 	return {
-		increment: () => {
-			return init + 1;
-		},
-		decrement: () => {
-			return init - 1;
-		},
+		increment: () => ++newVal,
+		decrement: () => --newVal,
 		reset: () => {
+			newVal = init;
 			return init;
 		},
 	};
 }
-/**
- * const counter = createCounter(5)
- * counter.increment(); // 6
- * counter.reset(); // 5
- * counter.decrement(); // 4
- */
 
 const counter = createCounter(5);
 console.log(counter?.increment());
 console.log(counter?.reset());
 console.log(counter?.decrement());
+
+["increment", "reset", "decrement"];
+/* 
+Use Testcase
+Output
+[6,5,5] */
+
+// expected [6,5,4]
