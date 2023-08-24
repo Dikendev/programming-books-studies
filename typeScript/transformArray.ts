f3e1fa603f4c4ed604c5930b0b3b4de940280bc0
Dikendev <diken.dev@gmail.com>
2023-08-23 22:44:56 -0300

    transform array

--- typeScript/transformArray.ts
+++ typeScript/transformArray.ts
@@ -1,9 +1,25 @@
 function map(arr: number[], fn: (n: number, i: number) => number): number[] {
-	return arr.map((n, i) => fn(n, i));
+	let newArr: number[] = [];
+
+	for (let i = 0; i < arr.length; i++) {
+		newArr[i] = fn(arr[i], i);
+	}
+
+	return newArr;
 }
+
+/* function map(arr: number[], fn: (n: number, i: number) => number): number[] {
+	let newArr: number[] = [];
+
+	for (let i = 0; i < arr.length; i++) {
+		newArr[i] = fn(arr[i], i);
+	}
+
+	return newArr;
+} */
 
 const plusone = (n: number, i: number) => n + 1;
 const plusI = (n: number, i: number): any => n + i;
 const constant = () => 42;
 const arr = [1, 2, 3];
-console.log(map(arr, plusone));
+console.log(map(arr, plusI));
