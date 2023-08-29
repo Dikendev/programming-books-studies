2cd2d531e50e9857403bca343591add2a7955d48
Dikendev <diken.dev@gmail.com>
2023-08-28 23:51:04 -0300

    string methods

--- typeScript/strings.ts
+++ typeScript/strings.ts
@@ -1,5 +1,13 @@
 let stringTest = "String to test";
-
 let newText = stringTest.charAt(0).toLowerCase() + stringTest.slice(1);
+// console.log(newText);
+let charCode = stringTest.charCodeAt(1);
+console.log(charCode);
 
-console.log(newText);
+let text1 = "Hello",
+	text2 = "World";
+let concatenate = text1.concat(` ${text2}`);
+console.log(concatenate);
+
+let indexOf = stringTest.indexOf("Test");
+console.log("indexOf", indexOf);
