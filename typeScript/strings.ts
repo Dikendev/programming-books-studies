7bc44c36a32ec56820d0ef46deb30049f461292a
Dikendev <diken.dev@gmail.com>
2023-08-29 21:53:19 -0300

    replace method

--- typeScript/strings.ts
+++ typeScript/strings.ts
@@ -2,12 +2,21 @@ let stringTest = "String to test";
 let newText = stringTest.charAt(0).toLowerCase() + stringTest.slice(1);
 // console.log(newText);
 let charCode = stringTest.charCodeAt(1);
-console.log(charCode);
+// console.log(charCode);
 
 let text1 = "Hello",
 	text2 = "World";
 let concatenate = text1.concat(` ${text2}`);
-console.log(concatenate);
+// console.log(concatenate);
 
 let indexOf = stringTest.indexOf("Test");
-console.log("indexOf", indexOf);
+// console.log("indexOf", indexOf);
+
+let re = /apples/gi;
+let str = "Apples are round, and apples are juice";
+let newstr = str.replace(re, "oranges");
+
+let re2 = /(\w+)\s(\w+)/;
+let str2 = "zara ali";
+let newStr2 = str2.replace(re, "$2, $1");
+console.log(newStr2);
