b3d25afb15f3f364cec0c47062978f4ae80f0793
Dikendev <diken.dev@gmail.com>
2023-08-30 21:02:55 -0300

    string methods

--- typeScript/strings.ts
+++ typeScript/strings.ts
@@ -16,15 +16,20 @@ let re = /apples/gi;
 let str = "Apples are round, and apples are juice";
 let newstr = str.replace(re, "oranges");
 
-let re2 = /(\w+)\s(\w+)/;
+let regex = /(\w+)\s(\w+)/;
 let str2 = "zara ali";
 let newStr2 = str2.replace(re, "$2, $1");
 console.log(newStr2);
 
 let str3 = "Apples are round, and apples are juicy";
 let srt3 = "Apples are round, and apples are juicy";
-if (str.search(re) === -1) {
+if (str.search(regex) === -1) {
 	console.log("Does not contains appels");
 } else {
 	console.log("Contains Apples");
 }
+
+let regex1 = /`123`/gi;
+let string1 = "123 are round, and apples are juicy";
+let newstr1 = string1.search(regex1);
+console.log(`oi`, newstr1);
