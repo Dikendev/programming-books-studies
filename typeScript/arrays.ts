1cb5994c4ff189bf3abb28480edaa52d520bcf6f
Dikendev <diken.dev@gmail.com>
2023-08-30 21:42:30 -0300

    every method

--- typeScript/arrays.ts
+++ typeScript/arrays.ts
@@ -12,7 +12,19 @@ let namesString = "Mary Tom, Jack, Jill";
 let names: string[] = new Array(namesString);
 
 for (let i = 0; i < names.length; i++) {
-	console.log(names[i]);
+	// console.log(names[i]);
 }
 
-// Concat() = Method returns a new array comprised of this array joined with two or more arrays.
+// concat() = Method returns a new array comprised of this array joined with two or more arrays. (pure method)
+const numeric: number[] = [1, 2, 3];
+const numericComp = ["1", "1", "1"].map(Number);
+const newNumbers = numeric.concat(numericComp);
+// console.log(newNumbers);
+
+// every() = method test whether all the elements in an array passes the test implemented by the provided function.
+function isBigEnough(element: number, index: number, array: number[]): boolean {
+	return element >= 10;
+}
+
+let passed: boolean = [12, 5, 6, 130, 44].every(isBigEnough);
+// console.log("Test Value:", passed);
