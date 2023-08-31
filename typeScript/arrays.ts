d9b347efc79840537c761c226543e015aefeeb9f
Dikendev <diken.dev@gmail.com>
2023-08-30 21:12:11 -0300

    Array object

--- typeScript/arrays.ts
+++ typeScript/arrays.ts
@@ -1,3 +1,16 @@
 let alphas: string[];
 alphas = ["1", "2", "3", "4"];
-console.log(alphas[1]);
+
+let arr_names: number[] = new Array(4);
+for (let i = 0; i < arr_names.length; i++) {
+	arr_names[i] = i * 2;
+	// console.log(arr_names[i]);
+}
+// console.log(arr_names);
+
+let namesString = "Mary Tom, Jack, Jill";
+let names: string[] = new Array(namesString);
+
+for (let i = 0; i < names.length; i++) {
+	console.log(names[i]);
+}
