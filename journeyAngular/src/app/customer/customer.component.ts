231365294dd8b6de90f150b110af30e970a9abfb
Dikendev <diken.dev@gmail.com>
2023-09-14 23:15:28 -0300

    Property binding

--- journeyAngular/src/app/customer/customer.component.ts
+++ journeyAngular/src/app/customer/customer.component.ts
@@ -7,10 +7,24 @@ import { Customer } from "./customer";
 })
 export class CustomerComponent {
 	Customers: Array<Customer>;
+	isAddNew: boolean;
 	constructor() {
+		this.isAddNew = false;
 		this.Customers = new Array<Customer>();
 		this.Customers.push(new Customer("Diego", "Kennedy"));
-		this.Customers.push(new Customer("Cristina", "Freitas"));
+		this.Customers.push(new Customer("Crbistina", "Freitas"));
 		this.Customers.push(new Customer("Joy", "dog"));
+	}
+
+	save(): void {
+		this.Customers.push(new Customer("David", "Michel"));
+	}
+
+	AddNew(): void {
+		this.isAddNew = true;
+	}
+
+	CancelSubmit(): void {
+		this.isAddNew = false;
 	}
 }
