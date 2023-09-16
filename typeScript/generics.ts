c182008be094fa6c7ba2de4a0d3518134593107a
Dikendev <diken.dev@gmail.com>
2023-09-16 13:24:06 -0300

    Generics 🔥

--- typeScript/generics.ts
+++ typeScript/generics.ts
@@ -1,8 +1,20 @@
 export {};
 
 function method<T>(args: T): T {
-	console.log(args);
 	return args;
 }
 method<number>(1);
 method<string>("hello generics");
+
+function meth<T>(arg: T[]): T[] {
+	console.log(arg.length);
+	return arg;
+}
+
+class CustomPerson extends Array {}
+class Person {}
+const people: Person[] = [];
+const newPerson = new CustomPerson();
+meth<Person>(people);
+meth<CustomPerson>(newPerson);
+const person = new Person();
