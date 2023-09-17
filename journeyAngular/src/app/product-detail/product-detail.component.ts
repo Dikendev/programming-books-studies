aee74289c26d6a8ac58688e0ea49a1ce869642a0
Dikendev <diken.dev@gmail.com>
2023-09-17 20:55:47 -0300

    Encapsulating CSS /  Change detection strategy

--- journeyAngular/src/app/product-detail/product-detail.component.ts
+++ journeyAngular/src/app/product-detail/product-detail.component.ts
@@ -1,17 +1,31 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import {
+	Component,
+	Input,
+	Output,
+	EventEmitter,
+	ViewEncapsulation,
+	ChangeDetectionStrategy,
+} from "@angular/core";
 
 @Component({
 	selector: "app-product-detail",
 	templateUrl: "./product-detail.component.html",
 	styleUrls: ["./product-detail.component.css"],
+	encapsulation: ViewEncapsulation.None,
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductDetailComponent {
 	@Input() name: string = "";
 
 	// event occur in child, and notify to the parent.
-	@Output() bought = new EventEmitter();
+	@Output() bought = new EventEmitter<string>();
 
 	buy() {
-		this.bought.emit();
+		this.bought.emit(this.name);
+	}
+
+	get productName(): string {
+		console.log(`Get ${this.name}`);
+		return this.name;
 	}
 }
