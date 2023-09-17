ee3bead6d5d01ebba035e091a453218e61be493d
Dikendev <diken.dev@gmail.com>
2023-09-16 22:05:09 -0300

    Imports and declarations 🔥

--- journeyAngular/src/app/app.module.ts
+++ journeyAngular/src/app/app.module.ts
@@ -5,10 +5,13 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CustomerComponent } from "./customer/customer.component";
 import { FormsModule } from "@angular/forms";
+import { ProductsModule } from "./products/products.module";
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
-	declarations: [AppComponent, CustomerComponent],
-	imports: [BrowserModule, AppRoutingModule, FormsModule],
+	declarations: [AppComponent, CustomerComponent, ProductListComponent, ProductDetailComponent],
+	imports: [BrowserModule, AppRoutingModule, FormsModule, ProductsModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
