86722479277b3692f1d6290012440dfec41c4a99
Dikendev <diken.dev@gmail.com>
2023-09-17 20:54:52 -0300

    Local reference variables 🔥

--- journeyAngular/src/app/product-list/product-list.component.ts
+++ journeyAngular/src/app/product-list/product-list.component.ts
@@ -8,7 +8,7 @@ import { Component } from "@angular/core";
 export class ProductListComponent {
 	selectedProduct: string = "";
 
-	onBuy() {
-		window.alert(`You just bought ${this.selectedProduct}!`);
+	onBuy(name: string) {
+		window.alert(`You just bought ${name}!`);
 	}
 }
