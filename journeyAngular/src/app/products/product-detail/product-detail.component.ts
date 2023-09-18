70f27f8d301c2bc3f0719bebef41e7af039a1752
Dikendev <diken.dev@gmail.com>
2023-09-17 23:16:19 -0300

    Interface and Pipes 🔥

--- journeyAngular/src/app/products/product-detail/product-detail.component.ts
+++ journeyAngular/src/app/products/product-detail/product-detail.component.ts
@@ -8,6 +8,7 @@ import {
 	OnChanges,
 	SimpleChanges,
 } from "@angular/core";
+import { Product } from "src/app/products/product.interface";
 
 @Component({
 	selector: "app-product-detail",
@@ -20,27 +21,24 @@ export class ProductDetailComponent implements OnChanges {
 	constructor() {}
 
 	ngOnInit(): void {
-		console.log(`Name is ${this.product} in the ngOnInit`);
+		console.log(`Name is ${this.product?.name} in the ngOnInit`);
 	}
 
-	ngOnChanges(changes: SimpleChanges): void {
-		const product = changes["product"];
-		const oldValue = product.previousValue;
-		const newValue = product.currentValue;
-		console.log(`Product changed from ${oldValue} to ${newValue}`);
-	}
-
-	@Input() product: string = "";
+	@Input() product: Product | undefined;
 
 	// event occur in child, and notify to the parent.
 	@Output() bought = new EventEmitter<string>();
 
-	buy() {
-		this.bought.emit(this.product);
+	ngOnChanges(changes: SimpleChanges): void {
+		const product = changes["product"];
+		if (!product.isFirstChange()) {
+			const oldValue = product.previousValue.name;
+			const newValue = product.currentValue.name;
+			console.log(`Product changed from ${oldValue} to ${newValue}`);
+		}
 	}
 
-	get productName(): string {
-		console.log(`Get ${this.product}`);
-		return this.product;
+	buy() {
+		this.bought.emit();
 	}
 }
