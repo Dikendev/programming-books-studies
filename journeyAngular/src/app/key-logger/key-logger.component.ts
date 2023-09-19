e13600b3c1e505e5c133217e03815d8ad5ef8bb1
Dikendev <diken.dev@gmail.com>
2023-09-19 19:23:38 -0300

    Observable, Reactive patterns 🔥

--- journeyAngular/src/app/key-logger/key-logger.component.ts
+++ journeyAngular/src/app/key-logger/key-logger.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { fromEvent } from "rxjs";
 
 @Component({
-  selector: 'app-key-logger',
-  templateUrl: './key-logger.component.html',
-  styleUrls: ['./key-logger.component.css']
+	selector: "app-key-logger",
+	templateUrl: "./key-logger.component.html",
+	styleUrls: ["./key-logger.component.css"],
 })
-export class KeyLoggerComponent {
+export class KeyLoggerComponent implements OnInit {
+	ngOnInit(): void {
+		const logger$ = fromEvent<KeyboardEvent>(
+			this.input?.nativeElement,
+			"keyup"
+		);
+		logger$.subscribe((event) => (this.keys += event.key));
+	}
 
+	@ViewChild("keyContainer", { static: true }) input: ElementRef | undefined;
+
+	keys: any = "";
 }
