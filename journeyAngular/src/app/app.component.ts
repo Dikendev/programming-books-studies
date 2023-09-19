b0b976549c122e4d914b48a812c09a042e6d7683
Dikendev <diken.dev@gmail.com>
2023-09-19 19:02:08 -0300

    Transforming object in Services

--- journeyAngular/src/app/app.component.ts
+++ journeyAngular/src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnDestroy } from "@angular/core";
+import { Component, Inject, OnDestroy } from "@angular/core";
+import { APP_CONFIG, AppConfig, appSettings } from "src/app.config";
 
 @Component({
 	selector: "app-root",
 	templateUrl: "./app.component.html",
+	providers: [{ provide: APP_CONFIG, useValue: appSettings }],
 })
 export class AppComponent implements OnDestroy {
 	title = "journeyAngular";
 
+	constructor(@Inject(APP_CONFIG) config: AppConfig) {}
 	ngOnDestroy(): void {}
 }
