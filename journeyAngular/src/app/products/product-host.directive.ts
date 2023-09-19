d4b3487e36f5b074d6c79a90493dfe89c7105121
Dikendev <diken.dev@gmail.com>
2023-09-18 21:01:08 -0300

    Dynamic Directives 🔥

--- journeyAngular/src/app/products/product-host.directive.ts
+++ journeyAngular/src/app/products/product-host.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, OnInit, ViewContainerRef } from "@angular/core";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
 
 @Directive({
-  selector: '[appProductHost]'
+	selector: "[appProductHost]",
 })
-export class ProductHostDirective {
+export class ProductHostDirective implements OnInit {
+	constructor(private viewContainer: ViewContainerRef) {}
 
-  constructor() { }
-
+	ngOnInit(): void {
+		const productRef = this.viewContainer.createComponent(
+			ProductDetailComponent
+		);
+		productRef.setInput("product", {
+			name: "Optical mouse",
+			price: 130,
+		});
+	}
 }
