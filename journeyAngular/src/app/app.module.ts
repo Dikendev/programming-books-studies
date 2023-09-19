26e85b8e288b13319c4a644fe02996bef1ffd2f7
Dikendev <diken.dev@gmail.com>
2023-09-19 19:14:02 -0300

    Observable, Reactive patterns 🔥

--- journeyAngular/src/app/app.module.ts
+++ journeyAngular/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { ProductsModule } from "./products/products.module";
 import { CopyrightDirective } from "./copyright.directive";
 import { NumericDirective } from './numeric.directive';
 import { MaintenanceComponent } from './maintenance/maintenance.component';
+import { KeyLoggerComponent } from './key-logger/key-logger.component';
 
 @NgModule({
-	declarations: [AppComponent, CopyrightDirective, NumericDirective, MaintenanceComponent],
+	declarations: [AppComponent, CopyrightDirective, NumericDirective, MaintenanceComponent, KeyLoggerComponent],
 	imports: [BrowserModule, AppRoutingModule, FormsModule, ProductsModule],
 	providers: [],
 	bootstrap: [AppComponent],
---
+++ journeyAngular/src/app/key-logger/key-logger.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-key-logger',
+  templateUrl: './key-logger.component.html',
+  styleUrls: ['./key-logger.component.css']
+})
+export class KeyLoggerComponent {
+
+}
