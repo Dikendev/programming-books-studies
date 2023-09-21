67ce6f884826c2b304c8b458d5cf4d2c59baaa44
Dikendev <diken.dev@gmail.com>
2023-09-20 22:54:22 -0300

    Product detail component

--- journeyAngular/src/app/products/product-detail/product-detail.component.ts
+++ journeyAngular/src/app/products/product-detail/product-detail.component.ts
@@ -8,7 +8,9 @@ import {
 	OnChanges,
 	SimpleChanges,
 } from "@angular/core";
+import { Observable } from "rxjs";
 import { Product } from "src/app/products/product.interface";
+import { ProductsService } from "../products.service";
 
 @Component({
 	selector: "app-product-detail",
@@ -18,20 +20,33 @@ import { Product } from "src/app/products/product.interface";
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductDetailComponent implements OnChanges {
+	constructor(private productsService: ProductsService) {}
+
+	@Input() id = -1;
 	@Input() product: Product | undefined;
 	// event occur in child, and notify to the parent.
 	@Output() bought = new EventEmitter<string>();
+	@Output() deleted = new EventEmitter<string>();
+	//Observable
+	product$: Observable<Product> | undefined;
 
-	ngOnChanges(changes: SimpleChanges): void {
-		const product = changes["product"];
-		if (!product.isFirstChange()) {
-			const oldValue = product.previousValue.name;
-			const newValue = product.currentValue.name;
-			console.log(`Product changed from ${oldValue} to ${newValue}`);
-		}
+	ngOnChanges(): void {
+		this.product$ = this.productsService.getProduct(this.id);
 	}
 
 	buy() {
 		this.bought.emit();
+	}
+
+	changePrice(product: Product, price: number) {
+		this.productsService.updateProduct(product.id, price).subscribe(() => {
+			alert(`The price of ${product.name} was changed!`);
+		});
+	}
+
+	remove(product: Product) {
+		this.productsService.deleteProduct(product.id).subscribe(() => {
+			this.deleted.emit();
+		});
 	}
 }
