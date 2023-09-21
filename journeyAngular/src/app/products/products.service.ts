7c5d74ddc8a0e2a4f06960fd7e52af30ecbaba1c
Dikendev <diken.dev@gmail.com>
2023-09-20 22:45:31 -0300

    CRUD operations

--- journeyAngular/src/app/products/products.service.ts
+++ journeyAngular/src/app/products/products.service.ts
@@ -1,20 +1,55 @@
 import { Injectable } from "@angular/core";
-import { Product } from "./product.interface";
-import { Observable, of } from "rxjs";
+import { Product, ProductDTO } from "./product.interface";
+import { HttpClient } from "@angular/common/http";
+import { Observable, map, of } from "rxjs";
 
 @Injectable({
 	providedIn: "root",
 })
 export class ProductsService {
-	constructor() {}
-
-	products: Product[] = [
-		{ name: "WebCam", price: 100 },
-		{ name: "Microphone", price: 200 },
-		{ name: "Wireless keyboard", price: 85 },
-	];
+	constructor(private http: HttpClient) {}
+	private productsUrl = "https://fakestoreapi.com/products";
 
 	getProducts(): Observable<Product[]> {
-		return of(this.products);
+		return this.http.get<ProductDTO[]>(this.productsUrl).pipe(
+			map((products) =>
+				products.map((product) => {
+					return this.convertToProduct(product);
+				})
+			)
+		);
+	}
+
+	getProduct(id: number): Observable<Product> {
+		return this.http
+			.get<ProductDTO>(`${this.productsUrl}/${id}`)
+			.pipe(map((product) => this.convertToProduct(product)));
+	}
+
+	addProduct(name: string, price: number): Observable<Product> {
+		return this.http
+			.post<ProductDTO>(this.productsUrl, {
+				title: name,
+				price: price,
+			})
+			.pipe(map((product) => this.convertToProduct(product)));
+	}
+
+	updateProduct(id: number, price: number): Observable<void> {
+		return this.http.patch<void>(`${this.productsUrl}/${id}`, {
+			price,
+		});
+	}
+
+	deleteProduct(id: number): Observable<void> {
+		return this.http.delete<void>(`${this.productsUrl}/${id}`);
+	}
+
+	private convertToProduct(product: ProductDTO): Product {
+		return {
+			id: product.id,
+			name: product.title,
+			price: product.price,
+		};
 	}
 }
