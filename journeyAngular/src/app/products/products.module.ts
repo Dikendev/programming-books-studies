f085e43caba2fb193521ef2e9737727668a11957
Dikendev <diken.dev@gmail.com>
2023-09-20 23:30:28 -0300

    fix modules

--- journeyAngular/src/app/products/products.module.ts
+++ journeyAngular/src/app/products/products.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductDetailComponent } from "./product-detail/product-detail.component";
 import { SortPipe } from "./sort.pipe";
-import { ProductHostDirective } from './product-host.directive';
-import { PermissionDirective } from './permission.directive';
-import { FavoritesComponent } from './favorites/favorites.component';
-import { ProductViewComponent } from './product-view/product-view.component';
-import { ProductCreateComponent } from './product-create/product-create.component';
+import { ProductViewComponent } from "./product-view/product-view.component";
+import { ProductCreateComponent } from "./product-create/product-create.component";
+import { ProductListComponent } from "./product-list/product-list.component";
 
 @NgModule({
-	declarations: [ProductListComponent, ProductDetailComponent, SortPipe, ProductHostDirective, PermissionDirective, FavoritesComponent, ProductViewComponent, ProductCreateComponent],
+	declarations: [
+		ProductListComponent,
+		ProductDetailComponent,
+		SortPipe,
+		ProductViewComponent,
+		ProductCreateComponent,
+	],
 	imports: [CommonModule],
 	exports: [ProductListComponent],
 })
