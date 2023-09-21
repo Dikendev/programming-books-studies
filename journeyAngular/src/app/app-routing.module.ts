bb342a0790293fad5449cd214073dbe499c6a324
Dikendev <diken.dev@gmail.com>
2023-09-20 23:59:36 -0300

    Routing working

--- journeyAngular/src/app/app-routing.module.ts
+++ journeyAngular/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { ProductListComponent } from "./products/product-list/product-list.component";
+import { CartComponent } from "./cart/cart.component";
 
-const routes: Routes = [{ path: "products", component: ProductListComponent }];
+const routes: Routes = [{ path: "cart", component: CartComponent }];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
