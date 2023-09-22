9e43cd0b1b74140d7453ef0bc3036bc60ad3dcf5
Dikendev <diken.dev@gmail.com>
2023-09-21 22:42:56 -0300

    Maintenance route

--- journeyAngular/src/app/app-routing.module.ts
+++ journeyAngular/src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { CartComponent } from "./cart/cart.component";
+import { MaintenanceComponent } from "./maintenance/maintenance.component";
+import { ProductDetailComponent } from "./products/product-detail/product-detail.component";
+import { ProductListComponent } from "./products/product-list/product-list.component";
+import { productDetailResolver } from "./products/product-detail.resolver";
 
-const routes: Routes = [{ path: "cart", component: CartComponent }];
+const routes: Routes = [
+	{
+		path: "products",
+		component: ProductListComponent,
+		children: [{ path: ":id", component: ProductDetailComponent }],
+	},
+
+	{ path: "cart", component: CartComponent },
+	{
+		path: "about",
+		loadChildren: () =>
+			import("./about/about.module").then((m) => m.AboutModule),
+	},
+	{
+		path: "products/:id",
+		component: ProductDetailComponent,
+		resolve: { product: productDetailResolver },
+	},
+	{ path: "", redirectTo: "/products", pathMatch: "full" },
+	{ path: "**", component: MaintenanceComponent },
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
--- journeyAngular/src/app/maintenance/maintenance.component.ts
+++ journeyAngular/src/app/maintenance/maintenance.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
 	selector: "app-maintenance",
@@ -6,7 +7,11 @@ import { Component, OnInit } from "@angular/core";
 	styleUrls: ["./maintenance.component.css"],
 })
 export class MaintenanceComponent implements OnInit {
-	constructor() {}
+	constructor(private router: Router) {}
 
 	ngOnInit(): void {}
+
+	goHome() {
+		this.router.navigate(["/"]);
+	}
 }
